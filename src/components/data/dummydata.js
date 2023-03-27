ea48671c7939d81761fa60d8695e5503357685bf
MuhammadAkmal7610 <98021630+MuhammadAkmal7610@users.noreply.github.com>
2023-03-27 10:26:12 +0500

    Add files via upload

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,233 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
+
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/services",
+    text: "Services",
+  },
+  {
+    url: "/portfolio",
+    text: "Portfolio",
+  },
+  {
+    url: "/testimonials",
+    text: "Testimonials",
+  },
+  
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "HELLO I'M",
+    name: "MUHAMMAD AKMAL",
+    post: "FRONTEND DEVELOPER",
+    design: "REACTJS  / UI DESIGNER",
+    desc: " Solution-driven Web developer adept at contributing to highly collaborative work enviroment and finding solutions. Proven experience in Html, Css, javascript,bootstrap and Reactjs. with good knowledge and best practises",
+  },
+]
+export const about = [
+  {
+    desc: "Solution-driven Web developer adept at contributing to highly collaborative work enviroment and finding solutions. Proven experience in Html, Css, javascript,bootstrap and Reactjs. with good knowledge and best practises",
+    desc1: "Solution-driven Web developer adept at contributing to highly collaborative work enviroment and finding solutions. Proven experience in Html, Css, javascript,bootstrap and Reactjs. with good knowledge and best practises",
+    cover: "./images/man.png",
+  },
+]
+export const services = [
+  {
+    id: 1,
+    icon: <Settings />,
+    title: "Creative Design",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 2,
+    icon: <CropRotate />,
+    title: "Clean Code",
+    desc: "Without errors code will be clean and fully tested code with desired output",
+  },
+  {
+    id: 3,
+    icon: <ViewInAr />,
+    title: "Responsive Design",
+    desc: "Design will be according to latest time and fully responsive using latest technologies",
+  },
+  {
+    id: 4,
+    icon: <PieChart />,
+    title: "Material UI	",
+    desc: "Material UI will be use for icons and also to improve the designe of website",
+  },
+  {
+    id: 5,
+    icon: <Code />,
+    title: "Bootstrap",
+    desc: "Bootsrape type technology also be used for responsive and also for designing the web pages",
+  },
+  {
+    id: 6,
+    icon: <BarChart />,
+    title: "Awesome Support",
+    desc: "After designing and developing the site I will provide all kind of support",
+  },
+]
+export const project = [
+  {
+    id: 1,
+    icon: <CloudOutlined />,
+    num: "89",
+    title: "HAPPY CLIENTS",
+  },
+  {
+    id: 2,
+    icon: <FavoriteBorder />,
+    num: "231",
+    title: "PROJECTS COMPLEATED",
+  },
+  {
+    id: 3,
+    icon: <Public />,
+    num: "108",
+    title: "FILES DOWNLOADED",
+  },
+  {
+    id: 4,
+    icon: <PersonOutlined />,
+    num: "1446",
+    title: "LINES OF CODE",
+  },
+]
+export const portfolio = [
+  {
+    id: 1,
+    cover: "../images/port/port1.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 2,
+    cover: "../images/port/port2.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 3,
+    cover: "../images/port/pexels-thisisengineering-3862632.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+  {
+    id: 4,
+    cover: "../images/port/port4.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 5,
+    cover: "../images/port/port5.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 6,
+    cover: "../images/port/pexels-serpstat-572056.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+]
+export const testimonials = [
+  {
+    id: 1,
+    text: "Solution-driven Web developer adept at contributing to highly collaborative work enviroment and finding solutions. Proven experience in Html, Css, javascript,bootstrap and Reactjs. with good knowledge and best practises",
+    image: "./images/testimonials/team-1.png",
+    name: "Muhammad Akmal",
+    post: "Front End Developer",
+  },
+  {
+    id: 2,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/team-2.png",
+    name: "Khurshid Khan",
+    post: "Back End Developer",
+  },
+  {
+    id: 3,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/team-3.png",
+    name: "Faiqa Anjum",
+    post: "React Developer",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    title: "Master These Awesome",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+    cover: "./images/blog/b1.png",
+  },
+  {
+    id: 2,
+    title: "Best Design Items to Appeal",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+    cover: "./images/blog/b2.png",
+  },
+  {
+    id: 3,
+    title: "The 20 Best Lightroom Presets",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+    cover: "./images/blog/b3.png",
+  },
+]
+export const contact = [
+  {
+    icon: <AddLocationAltOutlined />,
+    text1: "Bahawalnagar Punjab Pakistan",
+    text2: "Lahore Punjab Pakistan",
+  },
+  {
+    icon: <PhoneIphone />,
+    text1: "03039344761",
+    text2: "03484762198",
+  },
+  {
+    icon: <EmailOutlined />,
+    text1: "muhammadakmalbwn2019@gmail.com",
+    text2: "makmalk@mail.com",
+  },
+]
+export const social = [
+  {
+    icon: <Facebook />,
+  },
+  {
+    icon: <Twitter />,
+  },
+  {
+    icon: <Instagram />,
+  },
+  {
+    icon: <YouTube />,
+  },
+]
